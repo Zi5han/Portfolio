9a24d3692d382e28470d4bd8db2bf3a24df56a1a
Zi5han <84861306+Zi5han@users.noreply.github.com>
2024-12-27 17:11:09 +0100

    Addded Hero component
    - Updated Navbar styles for responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import styles from './App.module.css';
+import { About } from './components/About/About';
+import { Hero } from './components/Hero/Hero';
 import { Navbar } from './components/navbar/navbar';
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './Hero.module.css';
+import { getAssetPath } from '../../utils';
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Ada</h1>
+        <p className={styles.description}>
+          I'm a full-stack developer With 5 years of experience using React and
+          NodeJS. Reach out if you'd like to learn more!
+        </p>
+        <a className={styles.contactBtn} href="mailto:myemail@email.com">Contact me</a>
+      </div>
+      <img src={getAssetPath('hero/heroImage.png')} alt="Hero" className={styles.heroImg}/>
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
