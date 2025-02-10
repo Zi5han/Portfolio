300584f131e95ab3af06ad3872b666baec8e9fee
Zi5han <84861306+Zi5han@users.noreply.github.com>
2025-02-10 08:08:53 +0100

    Added missing id attribute to Projects component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,7 +6,7 @@ import { ProjectCard } from './ProjectCard';
 
 export const Projects = () => {
   return (
-    <section className={styles.container}>
+    <section className={styles.container} id='projects'>
       <h2 className={styles.title}>{'Projects'}</h2>
       <div className={styles.projectsContainer}>
         {projects.map((projectItem, index) => {
