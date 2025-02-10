76f1a841bd6f11cf34c694059c6724018204e17a
Zi5han <84861306+Zi5han@users.noreply.github.com>
2025-02-10 08:09:14 +0100

    Added Contact component

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import styles from './Contact.module.css';
+import { getAssetPath } from '../../utils';
+
+export const Contact = () => {
+  return (
+    <footer className={styles.container} id='contact'>
+      <div className={styles.contactText}>
+        <h2>{'Contact'}</h2>
+        <p>{'Feel free to reach out!'}</p>
+      </div>
+      <ul className={styles.contactInfo}>
+        <li className={styles.contactInfoEntry}>
+          <img src={getAssetPath('contact/emailIcon.png')} alt='Email Icon' />
+          <a href='mailto:myemail@email.com'>myemail@email.com</a>
+        </li>
+        <li className={styles.contactInfoEntry}>
+          <img
+            src={getAssetPath('contact/linkedinIcon.png')}
+            alt='LinkedIn Icon'
+          />
+          <a href='https://linkedin.com/myname'>linkedin.com/myname</a>
+        </li>
+        <li className={styles.contactInfoEntry}>
+          <img src={getAssetPath('contact/githubIcon.png')} alt='GitHub Icon' />
+          <a href='https://github.com/myname'>github.com/myname</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
