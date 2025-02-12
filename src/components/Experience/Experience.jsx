839fdba9d22b594d2c4e0e5a62f3f59ca6ad5f05
Zi5han <84861306+Zi5han@users.noreply.github.com>
2025-02-12 05:59:04 +0100

    Refactored Experience section to utilize new skills data structure and added ExperienceCategories component for better organization

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,56 +1,49 @@
-import React from 'react';
-
 import styles from './Experience.module.css';
-import skills from '../../data/skills.json';
+
+import new_skills from '../../data/new_skills.json';
 import history from '../../data/history.json';
+
 import { getAssetPath } from '../../utils';
+import { ExperienceCategories } from './ExperienceCategories';
 
 export const Experience = () => {
   return (
     <section className={styles.container} id='experience'>
       <h2 className={styles.title}>{'Experience and Carrier'}</h2>
       <div className={styles.content}>
-        <div className={styles.skillsContainer}>
-          {skills.map((skillItem, index) => {
-            return (
-              <div key={index} className={styles.skillItem}>
-                <div className={styles.skillImageContainer}>
-                  <img
-                    src={getAssetPath(skillItem.imageSrc)}
-                    alt={skillItem.title}
-                  />
-                </div>
-                <p>{skillItem.title}</p>
-              </div>
-            );
+        <div className={styles.skillsPanel}>
+          {new_skills.map((skillCategory, index1) => {
+            return <ExperienceCategories key={index1} skillCategory={skillCategory}/>;
           })}
         </div>
-        <ul className={styles.historyList}>
-          {history.map((historyItem, index) => {
-            return (
-              <li key={index} className={styles.historyItem}>
-                <img
-                  src={getAssetPath(historyItem.imageSrc)}
-                  alt={`${historyItem.organisation} Logo`}
-                />
-                <div className={styles.historyItemDetails}>
-                  <h3>{`${historyItem.role}`}</h3>
-                  <p
-                    className={styles.hisotryItemOrga}
-                  >{`${historyItem.organisation}`}</p>
-                  <p
-                    className={styles.historyItemDate}
-                  >{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
-                  <ul>
-                    {historyItem.experiences.map((experienceItem, index) => {
-                      return <li key={index}>{experienceItem}</li>;
-                    })}
-                  </ul>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+        <div className={styles.historyPanel}>
+          <ul className={styles.historyList}>
+            {history.map((historyItem, index) => {
+              return (
+                <li key={index} className={styles.historyItem}>
+                  <img
+                    src={getAssetPath(historyItem.imageSrc)}
+                    alt={`${historyItem.organisation} Logo`}
+                  />
+                  <div className={styles.historyItemDetails}>
+                    <h3>{`${historyItem.role}`}</h3>
+                    <p
+                      className={styles.hisotryItemOrga}
+                    >{`${historyItem.organisation}`}</p>
+                    <p
+                      className={styles.historyItemDate}
+                    >{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                    <ul>
+                      {historyItem.experiences.map((experienceItem, index) => {
+                        return <li key={index}>{experienceItem}</li>;
+                      })}
+                    </ul>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </section>
   );
---
+++ src/components/Experience/ExperienceCategories.jsx
@@ -0,0 +1,42 @@
+import styles from './ExperienceCategories.module.css';
+
+import PropTypes from 'prop-types';
+
+import { getAssetPath } from '../../utils';
+
+export const ExperienceCategories = ({
+  skillCategory: { category_title, skills }
+}) => {
+  return (
+    <div className={styles.categoryContainer}>
+      <h3 className={styles.categoryTitle}>{category_title}</h3>
+      <div className={styles.skillsContainer}>
+        {skills.map((skillItem, index2) => {
+          return (
+            <div key={index2} className={styles.skillItem}>
+              <div className={styles.skillImageContainer}>
+                <img
+                  src={getAssetPath(skillItem.imageSrc)}
+                  alt={skillItem.title}
+                />
+              </div>
+              <p>{skillItem.title}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+ExperienceCategories.propTypes = {
+  skillCategory: PropTypes.shape({
+    category_title: PropTypes.string.isRequired,
+    skills: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        imageSrc: PropTypes.string.isRequired
+      })
+    ).isRequired
+  }).isRequired
+};
