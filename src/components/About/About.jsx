afb8dbfc424972231151fdfa64bed6429d1d8265
Zi5han <84861306+Zi5han@users.noreply.github.com>
2025-02-12 05:58:34 +0100

    Refactored About section to dynamically render content from JSON

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,13 @@
 import styles from './About.module.css';
+
+import about from '../../data/about.json';
+
 import { getAssetPath as getImageURL } from '../../utils';
 
 export const About = () => {
   return (
     <section className={styles.container} id='about'>
-      <h2 className={styles.title}>{'About'}</h2>
+      <h2 className={styles.title}>{'About Me'}</h2>
       <div className={styles.content}>
         <img
           src={getImageURL('about/aboutImage.png')}
@@ -12,39 +15,17 @@ export const About = () => {
           className={styles.aboutImage}
         />
         <ul className={styles.aboutList}>
-          <li className={styles.aboutItem}>
-            <img src={getImageURL('about/cursorIcon.png')} alt='Cursor icon' />
-            <div className={styles.aboutItemText}>
-              <h3>{'Frontend Developer'}</h3>
-              <p>
-                {
-                  "I'm a front-end developer With experience in building responsive and optimized sites"
-                }
-              </p>
-            </div>
-          </li>
-          <li className={styles.aboutItem}>
-            <img src={getImageURL('about/serverIcon.png')} alt='Server icon' />
-            <div className={styles.aboutItemText}>
-              <h3>{'Backend Developer'}</h3>
-              <p>
-                {
-                  'I have experience developing fast and optimised back-end systems and APIs'
-                }
-              </p>
-            </div>
-          </li>
-          <li className={styles.aboutItem}>
-            <img src={getImageURL('about/cursorIcon.png')} alt='UI icon' />
-            <div className={styles.aboutItemText}>
-              <h3>{'UI Designer'}</h3>
-              <p>
-                {
-                  'I have designed multiple landing pages and have created design systems as well'
-                }
-              </p>
-            </div>
-          </li>
+          {about.map((aboutItem, index) => {
+            return (
+              <li className={styles.aboutItem} key={index}>
+              <img src={getImageURL(aboutItem.imagePath)} alt='Cursor icon' />
+              <div className={styles.aboutItemText}>
+                <h3>{aboutItem.title}</h3>
+                <p dangerouslySetInnerHTML={{ __html: aboutItem.content }}></p>
+              </div>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </section>
