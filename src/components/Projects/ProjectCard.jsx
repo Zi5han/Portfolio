c546ebd99a2ec735bb072cc9d14febfdc3be042f
Zi5han <84861306+Zi5han@users.noreply.github.com>
2025-02-12 05:57:30 +0100

    Reworked projects section, added custom scrollbar

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-
 import styles from './ProjectCard.module.css';
+
 import PropTypes from 'prop-types';
+
 import { getAssetPath } from '../../utils';
 
 export const ProjectCard = ({
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-
 import styles from './Projects.module.css';
+
 import projects from '../../data/projects.json';
+
 import { ProjectCard } from './ProjectCard';
 
 export const Projects = () => {
   return (
     <section className={styles.container} id='projects'>
       <h2 className={styles.title}>{'Projects'}</h2>
-      <div className={styles.projectsContainer}>
-        {projects.map((projectItem, index) => {
-          return <ProjectCard key={index} projectItem={projectItem} />;
-        })}
+      <div className={styles.projectsContainerWrapper}>
+        <div className={styles.projectsContainer}>
+          {projects.map((projectItem, index) => {
+            return <ProjectCard key={index} projectItem={projectItem} />;
+          })}
+        </div>
       </div>
     </section>
   );
