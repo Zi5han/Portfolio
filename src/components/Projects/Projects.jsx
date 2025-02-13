ff98345a9f8aa2c862dd78fe1fafc2f9217d782b
Zi5han <84861306+Zi5han@users.noreply.github.com>
2025-02-13 13:19:28 +0100

    Implemented horizontal scroll for projects section and projects section wrapper

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,3 +1,5 @@
+import { useRef, useEffect } from 'react';
+
 import styles from './Projects.module.css';
 
 import projects from '../../data/projects.json';
@@ -5,15 +7,32 @@ import projects from '../../data/projects.json';
 import { ProjectCard } from './ProjectCard';
 
 export const Projects = () => {
+  const projectsContainerRef = useRef(null);
+
+  useEffect(() => {
+    const projectsContainer = projectsContainerRef.current;
+    if (projectsContainer) {
+      const onWheel = (e) => {
+        const isScrollAtStart = projectsContainer.scrollLeft == 0;
+        const isScrollAtEnd = projectsContainer.scrollLeft >= projectsContainer.scrollWidth - projectsContainer.clientWidth - 15;
+        console.log(projectsContainer.scrollLeft);
+        if (!(isScrollAtStart && e.deltaY < 0) && !(isScrollAtEnd && e.deltaY > 0)) {
+          e.preventDefault();
+          projectsContainer.scrollLeft += e.deltaY;
+        }
+      };
+      projectsContainer.addEventListener('wheel', onWheel);
+      return () => projectsContainer.removeEventListener('wheel', onWheel);
+    }
+  });
+
   return (
     <section className={styles.container} id='projects'>
       <h2 className={styles.title}>{'Projects'}</h2>
-      <div className={styles.projectsContainerWrapper}>
-        <div className={styles.projectsContainer}>
-          {projects.map((projectItem, index) => {
-            return <ProjectCard key={index} projectItem={projectItem} />;
-          })}
-        </div>
+      <div className={styles.projectsContainer} ref={projectsContainerRef}>
+        {projects.map((projectItem, index) => {
+          return <ProjectCard key={index} projectItem={projectItem} />;
+        })}
       </div>
     </section>
   );
