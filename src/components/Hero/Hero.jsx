621636aacf767d4d998dfa22319cfd0cd59fefda
Zi5han <84861306+Zi5han@users.noreply.github.com>
2025-02-14 16:11:13 +0100

    Refactor layout and styling for overflow handling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,22 +5,16 @@ import { getAssetPath } from '../../utils';
 export const Hero = () => {
   return (
     <section className={styles.container}>
-      <div className={styles.content}>
-        <h1 className={styles.mainTitle}>{"Hey, I'm Zishan"}</h1>
-        <p className={styles.description}>
-          {
-            "I'm an aspiring game developer with knowledge in different areas. Check out my projects."
-          }
-        </p>
-        <a className={styles.contactBtn} href='mailto:trayten01@gmail.com'>
-          {'Contact me'}
-        </a>
+      <div className={styles.contentWrapper}>
+        <div className={styles.content}>
+          <h1 className={styles.mainTitle}>{"Hey, I'm Zishan"}</h1>
+          <p className={styles.description}>{"I'm an aspiring game developer with knowledge in different areas. Check out my projects."}</p>
+          <a className={styles.contactBtn} href='mailto:trayten01@gmail.com'>
+            {'Contact me'}
+          </a>
+        </div>
+        <img src={getAssetPath('hero/zi5han_pic.png')} alt='Zishan Profile Picture' className={styles.heroImg} />
       </div>
-      <img
-        src={getAssetPath('hero/zi5han_pic.png')}
-        alt='Zishan Profile Picture'
-        className={styles.heroImg}
-      />
       <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
     </section>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,42 +8,40 @@ export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className={styles.navbar}>
-      <a className={styles.pageTitle} href='/'>
-        {"Zi5han"}
-      </a>
-      <div className={styles.menu}>
-        <img
-          className={styles.menuBtn}
-          src={
-            menuOpen
-              ? getAssetPath('nav/closeIcon.png')
-              : getAssetPath('nav/menuIcon.png')
-          }
-          onClick={() => setMenuOpen(!menuOpen)}
-          alt='menu-button'
-        />
-        <ul className={`${styles.menuItem} ${menuOpen && styles.menuOpen}`}>
-          <li>
-            <a href='#about' onClick={() => setMenuOpen(false)}>
-              {'About'}
-            </a>
-          </li>
-          <li>
-            <a href='#experience' onClick={() => setMenuOpen(false)}>
-              {'Experience'}
-            </a>
-          </li>
-          <li>
-            <a href='#projects' onClick={() => setMenuOpen(false)}>
-              {'Projects'}
-            </a>
-          </li>
-          <li>
-            <a href='#contact' onClick={() => setMenuOpen(false)}>
-              {'Contact'}
-            </a>
-          </li>
-        </ul>
+      <div className={styles.navbarWrapper}>
+        <a className={styles.pageTitle} href='/'>
+          {'Zi5han'}
+        </a>
+        <div className={styles.menu}>
+          <img
+            className={styles.menuBtn}
+            src={menuOpen ? getAssetPath('nav/closeIcon.png') : getAssetPath('nav/menuIcon.png')}
+            onClick={() => setMenuOpen(!menuOpen)}
+            alt='menu-button'
+          />
+          <ul className={`${styles.menuItem} ${menuOpen && styles.menuOpen}`}>
+            <li>
+              <a href='#about' onClick={() => setMenuOpen(false)}>
+                {'About'}
+              </a>
+            </li>
+            <li>
+              <a href='#experience' onClick={() => setMenuOpen(false)}>
+                {'Experience'}
+              </a>
+            </li>
+            <li>
+              <a href='#projects' onClick={() => setMenuOpen(false)}>
+                {'Projects'}
+              </a>
+            </li>
+            <li>
+              <a href='#contact' onClick={() => setMenuOpen(false)}>
+                {'Contact'}
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
